e673371bdce2eb4706eacef3e7c16a997e6aee39
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-04 18:54:10 -0400

    Create Ejercicio 6.cpp

---
+++ Ejercicio 6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+ char n = 'J';
+    int age = 49;
+    int birth_year = 1440;
+    char name [] = "Jesus";
+    
+    int year=birth_year+age;
+    printf("%s cumplirá %i años en %i.",name, age , year);
+
+    return 0;
+}
