82a4f0286e0a71ea789c02c141525cb1e90e39b9
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-04 18:50:53 -0400

    Create EJERCIO 3 .cpp

---
+++ EJERCIO 3 .cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    char letter;
+    char task;   
+    letter = 'R';  
+    task = 'T';    
+    cout <<"Se solicita al robot "<< letter;
+    cout<<" para "<< task << ".";
+
+
+    return 0;
+}
