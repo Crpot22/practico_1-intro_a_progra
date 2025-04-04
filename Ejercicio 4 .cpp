1c5b03ff5cabab553fb4264d9b02d62b43bf4957
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-04 18:52:01 -0400

    Create Ejercicio 4 .cpp

---
+++ Ejercicio 4 .cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  char robot_name[]="RC-21\\\\M2";
+  cout<<"El robot "<<robot_name<< " tiene un mensaje."<<endl;
+  cout<<"El robot "<<robot_name<< " se esta incendiando."<<endl; 
+
+    return 0;
+}
