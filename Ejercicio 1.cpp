bd412850fbb94e5e9c028bcd4498abf9b7593b9c
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-04 18:31:52 -0400

    Create Ejercicio 1.cpp
    ejrcicio 1 practico1

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main() {
+    printf("_____________\n");
+    printf("|  /\"\"\"\"\"\\  |\n");
+    printf("| /       \\ |\n");
+    printf("| | 0   0 | |\n");
+    printf("| |   v   | |\n");
+    printf("| \\  ___  / |\n");
+    printf("|  \\_____/  |\n");  
+    printf("_____________\n");
+
+    return 0;
+}
